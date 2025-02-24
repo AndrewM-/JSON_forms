e0714f698d70457fa9df0f4f30ce1248470b436f
Andrew Mitchell <Andrew.Mitchell@westnet.com.au>
2025-02-25 10:28:53 +1100

    removing files from tracking

--- pads.py
+++
@@ -1,8 +0,0 @@
-
-def clearing_vegetation(j):
-    s = "Section Name"
-    return s
-        
-        
-if __name__ == "__main__":
-    print("I'm a library only")
--- work_areas.py
+++
@@ -1,8 +0,0 @@
-
-def clearing_vegetation(j):
-    s = str(j)
-    return s
-        
-        
-if __name__ == "__main__":
-    print("I'm a library only")
